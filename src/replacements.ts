98321be40bf75b15b26e4a0d208f2cfa912f1dc2
karma-works <christian@haegele.dev>
2021-08-23 19:07:23 +0200

    use raw strings

--- src/replacements.ts
+++ src/replacements.ts
@@ -1,11 +1,13 @@
 
 export class Replacement {
-    regex: RegExp;
+    regex: string;
+    modifier: string;
     replacement: string;
     description: string | undefined;
 
-    constructor(regex: RegExp, replacement: string, description: string | undefined) {
+    constructor(regex: string, modifier: string, replacement: string, description: string | undefined) {
         this.regex = regex;
+        this.modifier = modifier;
         this.replacement = replacement;
         this.description = description;
     }
@@ -18,20 +20,21 @@ export class Replacement {
 
 export default class Replacements {
 
-    private static _rmap: Array<Replacement> = [
-        new Replacement(/\b(z)(um[\/\*_\(-]zur|ur\[\/\*_\(-]zum)\b/ig, "\$1um", ""),
-        new Replacement(/jede[rnms]?[\/\*_\(-](jede[rnms]?)\b/ig,  "\$1", "")
+    private _rmap: Array<Replacement> = [
+        new Replacement(String.raw`\b(z)(um[\/\*_\(-]zur|ur\[\/\*_\(-]zum)\b`, "ig", "\$1um", ""),
+        new Replacement(String.raw`jede[rnms]?[\/\*_\(-](jede[rnms]?)\b`, "ig",  "\$1", "")
     ];
 
-    static rmap(): Array<Replacement> {
+    rmap(): Array<Replacement> {
         return this._rmap;
     }
 
     public replaceAll(inputString: string, incrementCounter: () => void): string {
         let outputString = inputString;
-        for (const replacement of Replacements.rmap()) {
-            if(replacement.regex.test(outputString)){
-                outputString = outputString.replace(replacement.regex, replacement.replacement);
+        for (const replacement of this.rmap()) {
+            let regex = RegExp(replacement.regex, replacement.modifier);
+            if(regex.test(outputString)){
+                outputString = outputString.replace(regex, replacement.replacement);
                 incrementCounter();
             }
         }
@@ -41,8 +44,9 @@ export default class Replacements {
 
     public getDebug(inputString: string): string {
         let out = '\n';
-        for (const replacement of Replacements.rmap()) {
-            if(replacement.regex.test(inputString)){
+        for (const replacement of this.rmap()) {
+            let regex = RegExp(replacement.regex, replacement.modifier);
+            if(regex.test(inputString)){
                 out = out + replacement + "\n";
             }
         }
